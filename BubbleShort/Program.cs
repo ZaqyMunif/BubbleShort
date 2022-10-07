73b860628b5e505155a34053e365010a36f46f0d
ZaqyMunif <zaqymunif17@gmail.com>
2022-10-07 17:28:33 +0700

    Membuat Mothod 1

--- BubbleShort/Program.cs
+++ BubbleShort/Program.cs
@@ -9,5 +9,32 @@ namespace BubbleShort
 
         // Deklarasi variabel int untuk menyiapkan banyaknya data pada array
         private int n;
+       
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data uang disimpan pada array
+            while(true)
+            {
+                Console.Write("Masukan banyaknya elemen pada Arra: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---------------");
+            Console.WriteLine("Masukan elemen Array");
+            Console.WriteLine("---------------");
+
+            //Pengguna memasukkan elemen pada array
+            for (int i = 0; i < n; i++) 
+            {
+                Console.Write("<" + (i +1) + "> ");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
