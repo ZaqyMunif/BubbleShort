c09d95b5cb91646a0fc51a150c8d1945c9478f34
ZaqyMunif <zaqymunif17@gmail.com>
2022-10-07 17:18:22 +0700

    Membuat Class

--- BubbleShort/Program.cs
+++ BubbleShort/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BubbleShort
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyiapkan banyaknya data pada array
+        private int n;
     }
 }
